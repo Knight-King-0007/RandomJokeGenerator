96418a46ce0aa0d13e753f036c5b48510d764e86
Tarun Jangra <tarunjangra2580@gmail.com>
2023-08-21 23:06:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const button = document.querySelector('.container button')
+const jokeText = document.querySelector('.container p')
+
+button.addEventListener('click', getjoke);
+
+
+function getjoke(){
+    fetch('https://icanhazdadjoke.com/', {
+        headers : {
+            'Accept': 'application/json'
+        }
+    }).then(data => data.json())
+    .then( obj => jokeText.innerHTML = obj.joke)
+} 
+
+
+
+//  async function getjoke(){
+//     const jokeData = await fetch('https://icanhazdadjoke.com/', {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     });
+//     const jokeObj =  await jokeData.json();
+//     jokeText.innerHTML = jokeObj.joke;
+// }
